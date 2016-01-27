e5ffb3e0c4c39bd930b199f096c2f6efedb5e6a3
pr0xmeh <jok3rpuff@gmail.com>
2016-01-27 00:22:52 -0500

    Dictionary file-system interface

---
+++ Flask/utils/fs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+anavah.utils.fs
+~~~~~~~~~~~~~~~
+
+File-system interface.
+
+:copyright: (c) 2016 Michael Hoyt. <@pr0xmeh>
+:license: Anavah.
+"""
+
+import os
+
+
+abs_current = os.path.dirname(os.path.realpath(__file__))
+abs_app = os.path.abspath(os.path.join(abs_current, os.pardir))
+
+abs_fs = {}
+for dir_path, dir_names, file_names in os.walk(abs_app, topdown=False):
+    dir_relative = dir_path.replace(abs_app, '')[1:] # Without beginning /
+    if '__pycache__' in dir_relative:
+        continue
+    for file_name in file_names:
+        if dir_relative.__len__() > 0:
+            key = '{}/{}'.format(dir_relative, file_name)
+        else:
+            key = file_name
+        abs_fs[key] = os.path.join(dir_path, file_name)
+
